eedc1c57711d3750e5fd9ccad08575c20069342b
Willy Weiskopf <william@weiskopf.me>
2014-04-04 01:33:37 -0600

    Correct spacing, considering new bars.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '496dc5ba001ffa0716337aac6a85d432');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8196f2bf42b9dde2f1846247b3148bef');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
