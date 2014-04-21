f27d1b20e1be7689b7bd03203f9bb556cb8e4f12
Willy Weiskopf <william@weiskopf.me>
2014-04-21 00:18:31 -0600

    Better responsiveness.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f599c0fa705d86b2e6723653aedf76c2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '86a9d7affc52a8acedccbac7eda8d406');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
