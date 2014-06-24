b5b360c53509bd0e2a2c726d9708320ad8721b46
Willy Weiskopf <william@weiskopf.me>
2014-06-23 23:06:23 -0600

    Styling for captions in galleries.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '86a9d7affc52a8acedccbac7eda8d406');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '71e70fbece35fb65276f4b62be459f7e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
