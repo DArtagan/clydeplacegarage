f413a6a3315719f4c6169a998f7bb06f6c3dbf69
Willy Weiskopf <william@weiskopf.me>
2014-07-22 03:47:43 -0600

    Declare those xs image resizes important.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7211343fd3a5a72b52eeb514365379f9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '64be306a3d1665239e9e174034256db1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
