ca2fb726a8cc5e0c2eec12531c22ece7b44b73cf
Willy Weiskopf <william@weiskopf.me>
2014-07-22 03:18:46 -0600

    White background for offcanvas.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '00a33344c5e4f9d9741beb5788dba1dd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9ea9884cf0d8ac86058894d272d88919');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
