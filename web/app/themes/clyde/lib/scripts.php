35820b90535fb7ea70710fb058ff58700477cd81
Willy Weiskopf <william@weiskopf.me>
2014-07-21 22:31:58 -0600

    Caption spacing, not thumbnail.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '92cbd9cba0b8069a4cc5d09a014f9d95');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a93ebe6ef6eff8d970f642db8bcf42b3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
