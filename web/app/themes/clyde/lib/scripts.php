1a1af8b392f454fcaf00c534bdaf3c5e2a890b71
Willy Weiskopf <william@weiskopf.me>
2014-07-22 03:59:32 -0600

    Do it for more images.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b4fbade378b812bbe5e941b0a0d61ff3');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c40e112883dad710125b0dbf1bdc11d0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
