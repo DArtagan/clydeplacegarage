3d706fa809e90a51f14cdf534e01fb3a6681fb7b
Willy Weiskopf <william@weiskopf.me>
2014-07-22 03:39:04 -0600

    Fixed the webkit right whitespace problem

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2249baaf3a464a522b486aa69d895b4a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fd46de8a798a9c0102ad8d7b097b74e1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- web/app/themes/clyde/templates/header-top-navbar.php
+++ web/app/themes/clyde/templates/header-top-navbar.php
@@ -1,7 +1,7 @@
 <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target=".navbar-collapse">
+      <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target=".sidebar-offcanvas">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -12,7 +12,7 @@
       </div>
     </div>
 
-    <div class="sidebar-offcanvas">
+    <div class="sidebar-offcanvas showhide">
       <nav class="collapse navbar-collapse showhide" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
