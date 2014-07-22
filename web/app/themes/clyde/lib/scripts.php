f882106893d456283c8869d9a3d2150317500ca7
Willy Weiskopf <william@weiskopf.me>
2014-07-21 22:28:35 -0600

    Caption spacing and alignment.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ac60e82e8d98e6076e062c5556345da9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f7bf93bdff61afa414bb1997360f5cb8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
