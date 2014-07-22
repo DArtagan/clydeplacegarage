5090f6a300d7129729c21ee8f8997fa2b097e77f
Willy Weiskopf <william@weiskopf.me>
2014-07-22 04:03:01 -0600

    And be careful of centereds.

--- web/app/themes/clyde/lib/scripts.php
+++ web/app/themes/clyde/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c40e112883dad710125b0dbf1bdc11d0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ce9a1d74e18d5d1bba9d77e7afe7560e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
